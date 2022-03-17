3b0aa3ee82ea0aa152f38d73ae147ff057e7edfa
yanni <yannick.ramond@datactors.com>
2022-03-17 18:13:35 +0100

    fix to listen to Data, not App

--- projects/4/src/server/server.js
+++ projects/4/src/server/server.js
@@ -1,4 +1,4 @@
-import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+import FlightSuretyData from '../../build/contracts/FlightSuretyData.json';
 import Config from './config.json';
 import Web3 from 'web3';
 import express from 'express';
@@ -7,10 +7,10 @@ import express from 'express';
 let config = Config['localhost'];
 let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
 web3.eth.defaultAccount = web3.eth.accounts[0];
-let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+let flightSuretyData = new web3.eth.Contract(FlightSuretyData.abi, config.dataAddress);
 
 
-flightSuretyApp.events.OracleRequest({
+flightSuretyData.events.OracleRequest({
     fromBlock: 0
   }, function (error, event) {
     if (error) console.log(error)
