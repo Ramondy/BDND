440e1eda94dc347525590e6a4c49bffcb9de294e
yanni <yannick.ramond@datactors.com>
2022-03-20 20:19:44 +0100

    verifier passes unit test

--- projects/5_capstone/eth/migrations/2_deploy_contracts.js
+++ projects/5_capstone/eth/migrations/2_deploy_contracts.js
@@ -1,17 +1,17 @@
 // migrating the appropriate contracts
-// var SquareVerifier = artifacts.require("./SquareVerifier.sol");
-// var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
-var CustomERC721Token = artifacts.require('CustomERC721Token');
+var Verifier = artifacts.require("./Verifier.sol");
+var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+// var CustomERC721Token = artifacts.require('CustomERC721Token');
 
 const name = "test_name";
 const symbol = "test_symbol";
 const baseTokenURI = "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/";
 
 module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier)
-      .then(() => {
-        return deployer.deploy(SolnSquareVerifier, name, symbol, baseTokenURI, SquareVerifier.address);
-      });
+  deployer.deploy(Verifier)
+      // .then(() => {
+      //   return deployer.deploy(SolnSquareVerifier, name, symbol, baseTokenURI, Verifier.address);
+      // });
 
   // deployer.deploy(CustomERC721Token, name, symbol, baseTokenURI);
 };
